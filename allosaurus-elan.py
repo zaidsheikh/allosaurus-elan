226186da60e8a1a384fefa881aef33a7e05b0dc3
Zaid Sheikh <zsheikh@cs.cmu.edu>
2021-08-23 21:23:41 -0400

    move audio conversion/slicing code to backend server

--- allosaurus-elan.py
+++ allosaurus-elan.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# A short script to that wraps the Allosaurus phoneme recognition system to
-# act as a local recognizer in ELAN.
-#
+# A short script invokes Allosaurus phoneme recognizer via
+# the CMU Linguistic Annotation Backend API
 
 import atexit
 import os
@@ -108,64 +107,74 @@ if os.path.exists(params.get('input_tier', '')):
 # mono 16KHz WAV, then load that temp file into pydub for easier exporting of
 # audio clips in the format that Allosaurus expects. 
 
-ffmpeg = shutil.which('ffmpeg')
-# TODO: move this to the backend
-if False and ffmpeg and not params['source'].endswith('.wav'):
-    print("PROGRESS: 0.2 Converting source audio", flush = True)
-    converted_audio_file = tempfile.NamedTemporaryFile(suffix = '.wav')
-    subprocess.call([ffmpeg, '-y', '-v', '0', \
-        '-i', params['source'], \
-        '-ac', '1',
-        '-ar', '16000',
-        '-sample_fmt', 's16',
-        '-acodec', 'pcm_s16le', \
-        converted_audio_file.name])
-else:
-    # Assume it's in the right format, allosaurus will let us know if it isn't
-    converted_audio_file = open(params['source'], mode='rb')
+# ffmpeg = shutil.which('ffmpeg')
+# # TODO: move this to the backend
+# if False and ffmpeg and not params['source'].endswith('.wav'):
+    # print("PROGRESS: 0.2 Converting source audio", flush = True)
+    # converted_audio_file = tempfile.NamedTemporaryFile(suffix = '.wav')
+    # subprocess.call([ffmpeg, '-y', '-v', '0', \
+        # '-i', params['source'], \
+        # '-ac', '1',
+        # '-ar', '16000',
+        # '-sample_fmt', 's16',
+        # '-acodec', 'pcm_s16le', \
+        # converted_audio_file.name])
 
-print(converted_audio_file.name)
-converted_audio = pydub.AudioSegment.from_file(converted_audio_file, format = 'wav')
-if not annotations:
-    annotations.append({'start': 0, 'end': len(converted_audio), 'value': ""})
+# Assume it's already in wav format, allosaurus will let us know if it isn't
+# converted_audio_file = open(params['source'], mode='rb')
+
+# print(converted_audio_file.name)
+# converted_audio = pydub.AudioSegment.from_file(converted_audio_file, format = 'wav')
+# if not annotations:
+    # annotations.append({'start': 0, 'end': len(converted_audio), 'value': ""})
 
 # Create a set of WAV clips for each of the annotations specified in
 # 'input_tier' in the format that Allosaurus expects
 
-print("PROGRESS: 0.3 Creating temporary clips", flush = True)
+# print("PROGRESS: 0.3 Creating temporary clips", flush = True)
 
 # TODO: use tempfile.TemporaryDirectory()
-tmp_dir = tempfile.mkdtemp(prefix="allosaurus-elan-")
-print(tmp_dir)
-
-for annotation in annotations:
-    annotation['clip'] = tempfile.NamedTemporaryFile(suffix = '.wav', dir = tmp_dir, delete=False)
-    clip = converted_audio[annotation['start']:annotation['end']]
-    clip.export(annotation['clip'], format = 'wav')
+# tmp_dir = tempfile.mkdtemp(prefix="allosaurus-elan-")
+# for annotation in annotations:
+    # annotation['clip'] = tempfile.NamedTemporaryFile(suffix = '.wav', dir = tmp_dir, delete=False)
+    # clip = converted_audio[annotation['start']:annotation['end']]
+    # clip.export(annotation['clip'], format = 'wav')
 
 
 print("PROGRESS: 0.9 Transcribing clips", flush = True)
-# allosaurus_transcription = allosaurus_model.recognize(params['source'])
-if not annotations:
-    # allosaurus_transcriptions = allosaurus_model.recognize(converted_audio_file.name, timestamp=True).split('\n')
-    pass
-else:
-    for annotation in annotations:
-        # annotation['transcription'] = allosaurus_model.recognize(annotation['clip'].name)
-        with open(annotation['clip'].name,'rb') as audio_file:
-            files = {'file': audio_file}
-            # url="http://localhost:8000/annotator/segment/1/annotate/2/"
-            url = params['server_url'].rstrip('/') + "/annotator/segment/1/annotate/2/"
-            try:
-                r = requests.post(url, files=files, data={})
-            except:
-                sys.stderr.write("Error connecting to backend server " + params['server_url'] + "\n")
-                traceback.print_exc()
-            print("Response from CMULAB server " + params['server_url'] + ": " + r.text)
-            annotation['transcription'] = json.loads(r.text)["transcription"]
-        # print(annotations['transcription'])
+with open(params['source'],'rb') as audio_file:
+    files = {'file': audio_file}
+    url = params['server_url'].rstrip('/') + "/annotator/segment/1/annotate/2/"
+    try:
+        r = requests.post(url, files=files, data={"segments": json.dumps(annotations)})
+    except:
+        sys.stderr.write("Error connecting to backend server " + params['server_url'] + "\n")
+        traceback.print_exc()
+    print("Response from CMULAB server " + params['server_url'] + ": " + r.text)
+    transcribed_annotations = json.loads(r.text)
+    # annotation['transcription'] = json.loads(r.text)["transcription"]
 
-converted_audio_file.close()
+# allosaurus_transcription = allosaurus_model.recognize(params['source'])
+
+# if not annotations:
+    # # allosaurus_transcriptions = allosaurus_model.recognize(converted_audio_file.name, timestamp=True).split('\n')
+    # pass
+# else:
+    # for annotation in annotations:
+        # # annotation['transcription'] = allosaurus_model.recognize(annotation['clip'].name)
+        # with open(annotation['clip'].name,'rb') as audio_file:
+            # files = {'file': audio_file}
+            # url = params['server_url'].rstrip('/') + "/annotator/segment/1/annotate/2/"
+            # try:
+                # r = requests.post(url, files=files, data={})
+            # except:
+                # sys.stderr.write("Error connecting to backend server " + params['server_url'] + "\n")
+                # traceback.print_exc()
+            # print("Response from CMULAB server " + params['server_url'] + ": " + r.text)
+            # annotation['transcription'] = json.loads(r.text)["transcription"]
+        # # print(annotations['transcription'])
+
+# converted_audio_file.close()
 
 
 # Then open 'output_tier' for writing, and return all of the new phoneme
@@ -178,19 +187,20 @@ with open(params['output_tier'], 'w', encoding = 'utf-8') as output_tier:
     output_tier.write('<TIER xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
                       'xsi:noNamespaceSchemaLocation="file:avatech-tier.xsd" columns="Allosaurus">\n')
 
-    if not annotations:
-        start = allosaurus_transcriptions[0].split()[0]
-        end = str(float(allosaurus_transcriptions[-1].split()[0]) +
-                  float(allosaurus_transcriptions[-1].split()[1]))
-        allosaurus_transcription = ''
-        for transcription in allosaurus_transcriptions:
-            allosaurus_transcription = allosaurus_transcription + ' ' + transcription.split()[-1]
+    # if not annotations:
+        # start = allosaurus_transcriptions[0].split()[0]
+        # end = str(float(allosaurus_transcriptions[-1].split()[0]) +
+                  # float(allosaurus_transcriptions[-1].split()[1]))
+        # allosaurus_transcription = ''
+        # for transcription in allosaurus_transcriptions:
+            # allosaurus_transcription = allosaurus_transcription + ' ' + transcription.split()[-1]
+        # output_tier.write('    <span start="%s" end="%s"><v>%s</v></span>\n' %
+                          # (start, end, allosaurus_transcription))
+    # else:
+        # for annotation in annotations:
+    for annotation in transcribed_annotations:
         output_tier.write('    <span start="%s" end="%s"><v>%s</v></span>\n' %
-                          (start, end, allosaurus_transcription))
-    else:
-        for annotation in annotations:
-            output_tier.write('    <span start="%s" end="%s"><v>%s</v></span>\n' %
-                              (annotation['start'], annotation['end'], annotation['transcription']))
+                          (annotation['start'], annotation['end'], annotation['transcription']))
 
     # start = allosaurus_transcriptions[0].split()[0]
     # prev_start = start
